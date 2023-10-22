2a15e5bc16e5261a4f8bba1398493f579d9d230e
Thiago Marques <thiagossmarques@gmail.com>
2023-10-22 20:20:30 -0300

    feat: insere configurações iniciais spring initializr

---
+++ src/test/java/dio/apirestsantander/ApiRestSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.apirestsantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
